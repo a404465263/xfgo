35bf20fb07390e65ce232a5fdb854f9a755e8904
locbytes <locbytes@gmail.com>
2018-08-27 13:12:57 +0800

    update to v1.2

--- README.md
+++ README.md
@@ -1,29 +1,39 @@
 # xfgo
-Fate/Grand Order B服过sign检测Xposed模块
-
-作者: LocBytes
+Fate/Grand Order b服过sign检测xposed模块
 
 当前游戏客户端版本: 1.21.2
 
+如有任何问题请提交issue，我会及时进行回复。
+
 ## 说明
 
-本模块工作原理为修改验证函数返回值，开启本模块后则必须使用科技，否则报201。
+**本模块工作原理为修改验证函数返回值或传入参数，开启本模块后则必须使用科技，否则报201。**
 
-如果不使用科技，请关闭此模块并重启手机。
+**如果不使用科技，请关闭此模块并重启手机。**
 
-B服FGO经测试无法在VirtualXposed中运行，并非本模块造成的问题。
+**B服FGO经测试无法在VirtualXposed中运行，并非本模块造成的问题。**
 
-Android版本需求: >=4.2.2
+Android版本需求: >=4.4.2
 
 Xposed API需求: >=54
 
 经测试可以通过[Magisk](https://forum.xda-developers.com/apps/magisk)以框中框的方式来使用本模块。
 
+因加入了撤退胜利的数据处理逻辑，导致需要使用JSONArray类，要求Android API最低为API-19，故将min SDK API更改为API-19。
+
+Android版本低于4.4.2的用户只能使用v1.1版的模块，即只能修改战斗数据，而无法使用撤退胜利。
+
+v1.1版：[https://github.com/locbytes/xfgo/releases/tag/v1.1](https://github.com/locbytes/xfgo/releases/tag/v1.1)
+
+**不使用撤退胜利功能的用户请使用v1.1版，使用新版会造成撤退时报错误201，想要使用撤退胜利功能的用户请等待自己科技的作者适配完成。**
+
 ## 使用方法
 
-1、安装并应用此模块。
+用户需安装并应用此模块。
 
-2、在科技服务端将`response`的`sign`改为`""`即可(由科技开发者进行操作)。
+### 修改战斗数据
+
+科技作者需在科技服务端将`response`的`sign`改为`""`。
 
 ```json
 {
@@ -33,7 +43,29 @@ Xposed API需求: >=54
 }
 ```
 
+### 撤退胜利
+
+科技作者需在科技服务端对request进行修改，修改方法，具体示例代码请先参考模块中的代码。
+
+先监测包含`key=battleresult`的`requestData`，对该`requestData`进行处理。
+
+拆分数据，获得`result`的value值，该值为一个json字符串。
+
+对该json数据进行处理，有如下数据需要修改
+
+```json
+{
+    "battleResult": 1,       // 胜利 Number
+    "elapsedTurn": 11,       // 回合数为11 Number
+    "aliveUniqueIds": []     // 无存活敌方 Array
+}
+```
+
+重新建立`requestData`，无需对sign进行处理。
+
 ## 科技服务端代码参考
+
+暂未添加撤退胜利的相关示例代码
 
 AnyProxy版: [https://github.com/locbytes/FGO_AnyProxy](https://github.com/locbytes/FGO_AnyProxy)
 
--- app/src/main/java/com/locbytes/xfgo/Main.java
+++ app/src/main/java/com/locbytes/xfgo/Main.java
@@ -1,8 +1,13 @@
 package com.locbytes.xfgo;
 
 import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
@@ -12,8 +17,62 @@ public class Main implements IXposedHookLoadPackage {
         if(lpparam.packageName.equals("com.bilibili.fatego")){
             findAndHookMethod("com.netease.htprotect.HTProtect",lpparam.classLoader,"getDataSign", Context.class, String.class, int.class, new XC_MethodHook() {
                 @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    if(param.args[1].toString().contains("battleresult")){
+                        //XposedBridge.log("Battle Result");
+                        String requestData=param.args[1].toString();
+                        String[] dataArray=requestData.split("&");
+                        /*
+                        int i=0;
+                        for (String aDataArray : dataArray) {
+                            XposedBridge.log(i+" "+aDataArray);
+                            ++i;
+                        }
+                        */
+
+                        // result
+                        //XposedBridge.log(dataArray[12]);
+                        JSONObject jsonObject = new JSONObject(dataArray[12].substring(7));
+                        //XposedBridge.log(jsonObject.get("battleResult").toString());
+                        //XposedBridge.log(jsonObject.get("aliveUniqueIds").toString());
+                        // battleResult=1 任务完成
+                        jsonObject.put("battleResult",1);
+                        // elapsedTurn=11 回合数11
+                        jsonObject.put("elapsedTurn",11);
+                        // 因JSONArray类要求，只能将最低Android SDK Version由API-17为API-19
+                        JSONArray aliveUniqueIds = jsonObject.getJSONArray("aliveUniqueIds");
+                        // aliveUniqueIds=[] 敌方无存活
+                        aliveUniqueIds = new JSONArray();
+                        //XposedBridge.log(aliveUniqueIds.toString());
+                        jsonObject.put("aliveUniqueIds",aliveUniqueIds);
+                        //XposedBridge.log(jsonObject.get("battleResult").toString());
+                        //XposedBridge.log(jsonObject.get("aliveUniqueIds").toString());
+                        //XposedBridge.log(jsonObject.toString());
+                        dataArray[12]="result="+jsonObject.toString();
+                        //XposedBridge.log(dataArray[12]);
+
+                        StringBuilder newRequestData = new StringBuilder();
+                        int i=1;
+                        for (String aDataArray : dataArray) {
+                            newRequestData.append(aDataArray);
+                            if(i<dataArray.length){
+                                newRequestData.append("&");
+                            }
+                            ++i;
+                        }
+
+                        //XposedBridge.log(requestData);
+                        //XposedBridge.log(newRequestData.toString());
+
+                        param.args[1]=newRequestData.toString();
+                    }
+                    super.beforeHookedMethod(param);
+                }
+
+                @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     if(param.args[1].toString().contains("battle_setup")||param.args[1].toString().contains("battle_resume")){
+                        //XposedBridge.log("Battle Setup/Resume");
                         param.setResult("");
                     }
                     super.afterHookedMethod(param);
