2e0c8b3f7af3f37a178f3a6d1f159457c16ac879
locbytes <locbytes@gmail.com>
2018-08-27 18:29:27 +0800

    对撤退胜利添加判断

--- app/src/main/java/com/locbytes/xfgo/Main.java
+++ app/src/main/java/com/locbytes/xfgo/Main.java
@@ -35,36 +35,45 @@ public class Main implements IXposedHookLoadPackage {
                         JSONObject jsonObject = new JSONObject(dataArray[12].substring(7));
                         //XposedBridge.log(jsonObject.get("battleResult").toString());
                         //XposedBridge.log(jsonObject.get("aliveUniqueIds").toString());
-                        // battleResult=1 任务完成
-                        jsonObject.put("battleResult",1);
-                        // elapsedTurn=11 回合数11
-                        jsonObject.put("elapsedTurn",11);
-                        // 因JSONArray类要求，只能将最低Android SDK Version由API-17为API-19
-                        JSONArray aliveUniqueIds = jsonObject.getJSONArray("aliveUniqueIds");
-                        // aliveUniqueIds=[] 敌方无存活
-                        aliveUniqueIds = new JSONArray();
-                        //XposedBridge.log(aliveUniqueIds.toString());
-                        jsonObject.put("aliveUniqueIds",aliveUniqueIds);
-                        //XposedBridge.log(jsonObject.get("battleResult").toString());
-                        //XposedBridge.log(jsonObject.get("aliveUniqueIds").toString());
-                        //XposedBridge.log(jsonObject.toString());
-                        dataArray[12]="result="+jsonObject.toString();
-                        //XposedBridge.log(dataArray[12]);
 
                         StringBuilder newRequestData = new StringBuilder();
-                        int i=1;
-                        for (String aDataArray : dataArray) {
-                            newRequestData.append(aDataArray);
-                            if(i<dataArray.length){
-                                newRequestData.append("&");
+
+                        // 检查是否为撤退
+                        Boolean battleCancel;
+                        if(jsonObject.getInt("battleResult")==3){
+                            battleCancel=true;
+                            // battleResult=1 任务完成
+                            jsonObject.put("battleResult",1);
+                            // elapsedTurn=11 回合数11
+                            jsonObject.put("elapsedTurn",11);
+                            // 因JSONArray类要求，只能将最低Android SDK Version由API-17为API-19
+                            JSONArray aliveUniqueIds = jsonObject.getJSONArray("aliveUniqueIds");
+                            // aliveUniqueIds=[] 敌方无存活
+                            aliveUniqueIds = new JSONArray();
+                            //XposedBridge.log(aliveUniqueIds.toString());
+                            jsonObject.put("aliveUniqueIds",aliveUniqueIds);
+                            //XposedBridge.log(jsonObject.get("battleResult").toString());
+                            //XposedBridge.log(jsonObject.get("aliveUniqueIds").toString());
+                            //XposedBridge.log(jsonObject.toString());
+                            dataArray[12]="result="+jsonObject.toString();
+                            //XposedBridge.log(dataArray[12]);
+                            int i=1;
+                            for (String aDataArray : dataArray) {
+                                newRequestData.append(aDataArray);
+                                if(i<dataArray.length){
+                                    newRequestData.append("&");
+                                }
+                                ++i;
                             }
-                            ++i;
+                            //XposedBridge.log(requestData);
+                            //XposedBridge.log(newRequestData.toString());
+                        }else{
+                            battleCancel=false;
                         }
 
-                        //XposedBridge.log(requestData);
-                        //XposedBridge.log(newRequestData.toString());
-
-                        param.args[1]=newRequestData.toString();
+                        if(battleCancel){
+                            param.args[1]=newRequestData.toString();
+                        }
                     }
                     super.beforeHookedMethod(param);
                 }
@@ -74,6 +83,10 @@ public class Main implements IXposedHookLoadPackage {
                     if(param.args[1].toString().contains("battle_setup")||param.args[1].toString().contains("battle_resume")){
                         //XposedBridge.log("Battle Setup/Resume");
                         param.setResult("");
+                    }
+                    if(param.args[1].toString().contains("battleresult")) {
+                        XposedBridge.log(param.args[1].toString());
+                        XposedBridge.log(param.getResult().toString());
                     }
                     super.afterHookedMethod(param);
                 }
